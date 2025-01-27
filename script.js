4afc3cb7236871e83a9f55a7b6ba477680bae009
Afere precious <preciousafere@gmail.com>
2025-01-27 14:51:16 +0100

    level - 100

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  const main = document.createElement("h2");
+  main.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.appendChild(main);
+
+  episodeList.forEach((episode) => {
+    const card = document.createElement("div");
+    card.className = "episode-card";
+
+    card.innerHTML = `<img src = "${episode.image.medium}"  alt = "${
+      episode.name
+    }">
+    <h3>${episode.name} (S${String(episode.season).padStart(2, "0")}E${String(
+      episode.number
+    ).padStart(2, "0")})</h3>
+    <p>${episode.summary}</p>`;
+
+    rootElem.appendChild(card);
+  });
 }
 
 window.onload = setup;
