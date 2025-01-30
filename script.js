883f0d62ad13ae80eed0523a8a6fac027aefe4ac
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2025-01-30 14:27:15 +0100

    Level 200 completed

--- script.js
+++ script.js
@@ -1,14 +1,132 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
+  selector(allEpisodes);
+  searchBox(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
+function selector(episodeList) {
+  const rootElem = document.getElementById("root");
 
+  // Bootstrap Selector Container
+  const selectorContainer = document.createElement("div");
+  selectorContainer.classList.add("container", "mt-4");
+
+  // Bootstrap Selector Row
+  const selectorRow = document.createElement("div");
+  selectorRow.classList.add("row");
+
+  // Bootstrap Col
+  const selectorCol = document.createElement("div");
+  selectorCol.classList.add("col-md-3", "mb-2");
+
+  // Selector
+  const selectorBox = document.createElement("select");
+  selectorBox.classList.add("form-control");
+  selectorBox.setAttribute("id", "episodeSelect");
+  selectorBox.innerHTML = '<option value="">Select Episode</option>';
+
+  // Populate the dropdown with episodes
+  episodeList.forEach((episode, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.textContent = `S${String(episode.season).padStart(2, "0")}E${String(
+      episode.number
+    ).padStart(2, "0")} - ${episode.name}`;
+    selectorBox.appendChild(option);
+  });
+
+  // Event listener for when an option is selected
+  selectorBox.addEventListener("change", (e) => {
+    const selectedEpisodeIndex = e.target.value;
+    const displayResults = document.querySelector(".result-col p");
+    if (selectedEpisodeIndex === "") {
+      // Show all episodes and update the result text
+      makePageForEpisodes(episodeList);
+      if (displayResults)
+        displayResults.textContent = `Displaying ${episodeList.length} results`;
+    } else {
+      // Show only selected episode and clear the result text
+      makePageForEpisodes([episodeList[selectedEpisodeIndex]]);
+      if (displayResults) displayResults.textContent = "";
+    }
+  });
+
+  // Append the selector box to the column and row
+  selectorCol.appendChild(selectorBox);
+  selectorRow.appendChild(selectorCol);
+  selectorContainer.appendChild(selectorRow);
+  rootElem.appendChild(selectorContainer);
+}
+
+//FUNCTION SEARCH BOX
+
+function searchBox(episodeList) {
+  const rootElem = document.getElementById("root");
+  // Bootstrap Search Container
+  const searchContainer = document.createElement("div");
+  searchContainer.classList.add("container", "mt-4");
+
+  // Bootstrap Search Row
+  const searchRow = document.createElement("div");
+  searchRow.classList.add("row");
+
+  //Bootstrap Col
+  const searchCol = document.createElement("div");
+  searchCol.classList.add("search-col", "col-md-3", "mb-2");
+
+  //Search
+  const searchBox = document.createElement("input");
+  searchBox.setAttribute("type", "text");
+  searchBox.setAttribute("placeholder", "Search");
+  searchBox.classList.add("form-control");
+
+  //Results Col
+  const resultsCol = document.createElement("div");
+  resultsCol.classList.add(
+    "result-col",
+    "col-md-3",
+    "text-md-right",
+    "text-muted"
+  );
+
+  //Display Results
+  const displayResults = document.createElement("p");
+  displayResults.textContent = `Displaying ${episodeList.length} results`;
+
+  searchBox.addEventListener("input", () => {
+    const searchInputData = searchBox.value.toLowerCase();
+    const filterEpisodes = episodeList.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(searchInputData) ||
+        episode.summary.toLowerCase().includes(searchInputData)
+      );
+    });
+    displayResults.textContent = `Displaying ${filterEpisodes.length} / ${episodeList.length}`;
+    makePageForEpisodes(filterEpisodes);
+  });
+  //Append
+  rootElem.appendChild(searchContainer);
+  searchContainer.appendChild(searchRow);
+  searchRow.appendChild(searchCol);
+  searchRow.appendChild(resultsCol);
+  searchCol.appendChild(searchBox);
+  resultsCol.appendChild(displayResults);
+}
+
+//---------MAKEPAGEFOREPISODES FUNCTION
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
+  //remove existing container before rerendering
+
+  const existingContainer = document.querySelector(".episodes-container");
+  if (existingContainer) {
+    existingContainer.remove();
+  }
+
   // Bootstrap Container
   const container = document.createElement("div");
-  container.classList.add("container", "mt-4");
+  container.classList.add("container", "mt-4", "episodes-container");
 
   // Bootstrap Row
   const row = document.createElement("div");
